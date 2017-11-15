d5e379a975f0f3341e3dd136365902adff6a7001
Elrey Belmonti and Megan Olson <elrey+megan@>
2017-11-15 08:59:48 -0800

    Set up routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'genres#index'
+
+  resources :genres do
+    resources :books
+  end
+
+  resources :books do
+    resources :reviews
+  end
+
 end
