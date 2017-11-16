8b9e7c040eaf274606935114db7b8d8362c1ac41
Elrey Belmonti and Megan Olson <elrey+megan@>
2017-11-15 16:24:49 -0800

    Add styling with panels and colors.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Review.destroy_all
 Book.destroy_all
 
 10.times do |index|
-  Genre.create!(name: Faker::Book.genre).books.create!(name: Faker::Book.title, bio: Faker::Lorem.paragraph).reviews.create!(rating: Faker::Number.between(1, 5), review: Faker::Lorem.paragraph)
+  Genre.create!(name: Faker::Book.genre).books.create!(name: Faker::Book.title, bio: Faker::Lorem.paragraph).reviews.create!(rating: Faker::Number.between(1, 5), review: Faker::Lorem.paragraph(3))
 end
 
 p "Created #{Genre.count} genres"
