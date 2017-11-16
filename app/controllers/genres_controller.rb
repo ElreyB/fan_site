bb15fe586e8a6db9932f4edfe1f2f320a628dd4d
Elrey Belmonti and Megan Olson <elrey+megan@>
2017-11-16 13:40:48 -0800

    change rating number to stars

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,11 +4,12 @@ class GenresController < ApplicationController
     @genres = Genre.all
     @books = Book.all
     @popular_books = Book.most_popular(@books)
-    @random_genre = Genre.random_genre
+    @random_genres = Genre.random_genre
     @popular_user = Book.find_popular_user
     @popular_user_reviews = Book.grab_user_reviews(@popular_user)
     @random_review = Book.get_random_review(@popular_user_reviews)
     @recent_reviews = Review.recent_reviews
+    @random_genre = @random_genres.books[rand(@random_genres.books.length)]
   end
 
   def show
